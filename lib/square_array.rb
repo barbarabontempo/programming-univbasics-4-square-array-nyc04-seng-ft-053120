e77d2da6fd9ea89087ef0b472ddf457b85360506
Barbara Bontempo <barbarabontempo94@gmail.com>
2020-06-08 12:41:12 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -9,12 +9,3 @@ def square_array(numbers)
   end
   puts new_numbers
 end
-
-
-def square_array(numbers)
-  new_numbers = []
-  numbers.length.times do |index|
-    new_numbers.push(numbers[index] ** 2)
-  end
-  puts new_numbers
-end
